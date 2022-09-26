9bbfc77519db6c69d8a1c683c50a3df5697108e7
KIIZA STEPHEN <53976517+Atwiinesteve@users.noreply.github.com>
2022-09-26 12:02:30 +0000

    Added Encoding Checker function.

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ function getTextResponseFromServerAsync(url, callback) {
   var request = new XMLHttpRequest();
   request.open('GET', url);
   request.onreadystatechange = function() {
-    if(request.readyState === 4 && request.status === 200) {
+    if (request.readyState === 4 && request.status === 200) {
       var responseType = request.getResponseHeader('Content-Type');
-      if(responseType.match(/^text/g)) {
+      if (responseType.match(/^text/g)) {
         callback(request.responseText)
       };
     };
@@ -29,8 +29,31 @@ function getTextResponseFromServerAsync(url, callback) {
 function getTextResponseFromServerSync(url, callback) {
   var request = new XMLHttpRequest();
   request.open('GET', url, false);
-  if(request.status !== 200) throw new Error(request.statusText);
+  if (request.status !== 200) throw new Error(request.statusText);
   var type = request.getResponseHeader('Content-Type');
-  if(!type.match(/^text/g)) throw new Error(`Expected Textual Type but got: ${type}`);
+  if (!type.match(/^text/g)) throw new Error(`Expected Textual Type but got: ${type}`);
   return request.responseText;
+};
+
+// Parsing the HTTP response
+function get(url, callback) {
+  try {
+    var request = new XMLHttpRequest();
+    request.open('GET', url, true);
+    request.onreadystatechange = function() {
+      if (request.readyState === 4 && request.status === 200) {
+        var type = request.getRequestHeader('Content-Type');
+        if (type.indexOf('xml') !== -1 && request.responseXML) {
+          callback(request.responseXML);
+        } else if (type === 'application/json') {
+          callback(JSON.parse(request.responseText))
+        } else {
+          callback(request.responseText);
+        };
+      };
+    };
+    request.send(null);
+  } catch (error) {
+    console.log({ name: error.name, message: error.message, stack: error.stack })
+  }
 };
