76a4f3553d86f3e1e04b691a02b243c8d87e6648
KIIZA STEPHEN <53976517+Atwiinesteve@users.noreply.github.com>
2022-09-26 11:19:08 +0000

    Added Server Response Function.

--- index.js
+++ index.js
@@ -9,3 +9,18 @@ function postMessage(msg) {
     console.log({ name: error.name, message: error.message, stack: error.stack })
   }
 };
+
+// Getting an HTTP response onreadystatechange
+function getTextFromServer(url, callback) {
+  var request = new XMLHttpRequest();
+  request.open('GET', url);
+  request.onreadystatechange = function() {
+    if(request.readyState === 4 && request.status === 200) {
+      var responseType = request.getResponseHeader('Content-Type');
+      if(responseType.match(/^text/g)) {
+        callback(request.responseText)
+      };
+    };
+  };
+  request.send(null);
+};
